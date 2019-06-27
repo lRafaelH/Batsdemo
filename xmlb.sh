b21f95db4aa6341ee477efe513816f473890df7d
lRafaelH <root@localhost.localdomain>
2019-06-27 12:01:11 +0800

    added zip file

--- xmlb.sh
+++ xmlb.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-com () {
+output () {
 bats $input | tail -n +2
 }
+
+duration="$({ time bats -t $input  >/dev/null; } 2>&1 | grep real | tr --delete real)"
+success="$(grep -c "ok" "$time".xml)
+time="$(date +"%d-%m-%Y")"
+
 echo "insert path of bats"
 read input
-
-echo "insert file name for your xml file"
-read file
-cat << EOF > $file
+cat << EOF > $time.xml
 <?xml version = "1.0" encoding = "utf-8"?>
 
-<testoutput>
-<testresult>
-$(com)
+<batstest duration=$(echo ${duration//[[:blank:]]/}) success= $success >
+<testcases totaltest=>
+$(output)
 </testresult>
 </testoutput>
 EOF
